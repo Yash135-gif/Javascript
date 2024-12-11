1b7fca5b0ab3abc7d9804753108901a60126b1b9
Yash135-gif <gyash4098@gmail.com>
2024-12-11 23:32:39 +0530

    new code

---
+++ Arrays/array.js
@@ -0,0 +1,5 @@
+function yash(){
+    let a="<h1>mera joota hai japani</h1>"
+    document.write(a)
+}
+
--- data.js
+++ data.js
@@ -38,125 +38,125 @@
 
     //   Answer 2---------------------------------------------------------------
 
-     let answer1=confirm("Do You Completed the course")
-     if(answer1==true){
+//      let answer1=confirm("Do You Completed the course")
+//      if(answer1==true){
         
-        let answer2=confirm("Do you have a certificate")
-        if(answer2==true){
-            alert("Completed")
-        }
-        else{
-            alert("Not Completed")
-        }
+//         let answer2=confirm("Do you have a certificate")
+//         if(answer2==true){
+//             alert("Completed")
+//         }
+//         else{
+//             alert("Not Completed")
+//         }
 
-     }
-     else{
-        alert("Not Completed")
-     }  
+//      }
+//      else{
+//         alert("Not Completed")
+//      }  
     
-        // ----------------------------------------------------------  Answer 3 -----------------------------------------------------------
+//         // ----------------------------------------------------------  Answer 3 -----------------------------------------------------------
         
-       let price=prompt("enter the price")
-       if(price>0)
-       {
-        let coupen=confirm("do you have a discount coupen")
-        if(coupen==true){
-              alert(price*10/100)
-        }   
-       }
+//        let price=prompt("enter the price")
+//        if(price>0)
+//        {
+//         let coupen=confirm("do you have a discount coupen")
+//         if(coupen==true){
+//               alert(price*10/100)
+//         }   
+//        }
          
-            // ----------------------------------------------------------  Answer 4 -----------------------------------------------------------
+//             // ----------------------------------------------------------  Answer 4 -----------------------------------------------------------
 
-    let marks=prompt("enter your marks")
-    if(marks<=33)
-    {
-        alert("You are fail")
-    }
-    else{
-        alert("you are pass")
-    }
+//     let marks=prompt("enter your marks")
+//     if(marks<=33)
+//     {
+//         alert("You are fail")
+//     }
+//     else{
+//         alert("you are pass")
+//     }
     
-          // ----------------------------------------------------------  Answer 6 -----------------------------------------------------------
+//           // ----------------------------------------------------------  Answer 6 -----------------------------------------------------------
 
-       let sure=confirm("are you sure to delete")
-       if(sure==true){
-          alert("Item Deleted ")
-       }
-       else{
-        alert("Cancelled")
-       }
+//        let sure=confirm("are you sure to delete")
+//        if(sure==true){
+//           alert("Item Deleted ")
+//        }
+//        else{
+//         alert("Cancelled")
+//        }
         
 
-         // ----------------------------------------------------------  Answer 8 -----------------------------------------------------------
+//          // ----------------------------------------------------------  Answer 8 -----------------------------------------------------------
 
-       let i=1
-       while(i<=10)
-       {
-         let ptag= "<p>"  +  i +  "</p>" 
-         document.write(ptag) 
-         i++
-       }
+//        let i=1
+//        while(i<=10)
+//        {
+//          let ptag= "<p>"  +  i +  "</p>" 
+//          document.write(ptag) 
+//          i++
+//        }
         
-     // ----------------------------------------------------------  Answer 9 -----------------------------------------------------------
+//      // ----------------------------------------------------------  Answer 9 -----------------------------------------------------------
         
-      let input;
-     do{
-        input=prompt("enter the number")
-     }while(input<10)
-            //  ----------------------------------------------------------  Answer 10 -----------------------------------------------------------
+//       let input;
+//      do{
+//         input=prompt("enter the number")
+//      }while(input<10)
+//             //  ----------------------------------------------------------  Answer 10 -----------------------------------------------------------
 
 
-    let day=parseInt(prompt("enter the day of the week"))
+//     let day=parseInt(prompt("enter the day of the week"))
      
-     switch(day){
-        case 1:
-         alert("Sunday")
+//      switch(day){
+//         case 1:
+//          alert("Sunday")
 
-        case 2:
-         alert("Monday")
+//         case 2:
+//          alert("Monday")
         
-        case 3:
-            alert("Tuesday")    
+//         case 3:
+//             alert("Tuesday")    
  
-        case 4:
-         alert("Wednesday")
+//         case 4:
+//          alert("Wednesday")
 
-        case 5:
-            alert("Thursday")
+//         case 5:
+//             alert("Thursday")
 
-        case 6:
-           alert("Friday")
+//         case 6:
+//            alert("Friday")
 
-        case 7:
-          alert("Saturday")
+//         case 7:
+//           alert("Saturday")
 
-        default:
-          alert("Invalid day")
+//         default:
+//           alert("Invalid day")
 
-    }
+//     }
 
-        //  Replace, ReplaceAll--------------------------------------------------   
+//         //  Replace, ReplaceAll--------------------------------------------------   
 
-       let text="the java mern Java and Javascript is fun"
-       let ans=text.replace(/java/ig,"Hello")
-       console.log(text);
+//        let text="the java mern Java and Javascript is fun"
+//        let ans=text.replace(/java/ig,"Hello")
+//        console.log(text);
 
-    //   Split--------------------------------------------------------------------
+//     //   Split--------------------------------------------------------------------
       
-    // photo ha iski
+//     // photo ha iski
 
 
-    //  template literals (``) -------------------------------------------------------
+//     //  template literals (``) -------------------------------------------------------
 
-    function yash(){
-        alert(`this "cybrom" 'office' string`)
-    }
+//     function yash(){
+//         alert(`this "cybrom" 'office' string`)
+//     }
    
-    // use of html tags in document.write----------------------------------------------
-    function cena(){
-        let para=" boost is the <br> Energy "
-        document.write(para)
-    }
+//     // use of html tags in document.write----------------------------------------------
+//     function cena(){
+//         let para=" boost is the <br> Energy "
+//         document.write(para)
+//     }
 
-   cena()
+//    cena()
      
