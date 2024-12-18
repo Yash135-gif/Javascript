3be3ee2c479846d669b428078ce75312cd158664
Yash135-gif <gyash4098@gmail.com>
2024-12-18 12:13:19 +0530

    new code

--- object/object.js
+++ object/object.js
@@ -1,27 +1,44 @@
-let arr=["yash",25,"Bhopal"]    //  arrow
-let text="this is a string";    // string
+// let arr=["yash",25,"Bhopal"]    //  arrow
+// let text="this is a string";    // string
 
-let obj={
-    name:"yash",
-    age:20,
-    city:"Bhopal",
-    welcome:function(){
-        console.log("welcome to my house");    // function bhi daal sakta hai object ma
-    }
+// let obj={
+//     name:"yash",
+//     age:20,
+//     city:"Bhopal",
+//     welcome:function(){
+//         console.log("welcome to my house");    // function bhi daal sakta hai object ma
+//     }
   
 
-}
+// }
    
-  obj.welcome()
- console.log(obj.name)      //object access
- console.log(obj.age)        //object access
- console.log(obj.city)      //object access 
+//   obj.welcome()
+//  console.log(obj.name)     //object access
+//  console.log(obj.age)        //object access
+//  console.log(obj.city)      //object access 
   
 
  
 
-      obj.email="yashg4884@gmail.com"    // new key:value; add karna ka liya.
-  console.log(obj)
+//       obj.email="yashg4884@gmail.com"    // new key:value; add karna ka liya.
+//   console.log(obj)
+    
+//     //   Delete karna-----------------------------------------
+
+//   delete obj.city;
+
+    // Spread Operators-----------------------------------------------
+   const yashA={nameA:"YashA", classA:"bro"};
+   const yashB={nameB:"YashB", classB:"bruh"};
+
+   const friends={...yashA,...yashB};
+   console.log(friends)
+
+        
+   
+     
+
+
 
 
 
