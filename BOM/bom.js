7f530c68f7f9760577bb7c4a52e5744e131270e0
Yash135-gif <gyash4098@gmail.com>
2025-01-16 20:07:56 +0530

    new code

--- BOM/bom.js
+++ BOM/bom.js
@@ -1,14 +1,18 @@
-function okk(){
+// function okk(){
 //    window.alert("you are human")
 //    let ans= window.prompt("did you like this website")
 //     window.alert(ans)
 //  let ans1=window.confirm("are you sure")
 //  window.alert(ans1)
- let age=window.prompt("what is your age")
- window.alert(age)
-}
+//  let age=window.prompt("what is your age")
+//  window.alert(age)
+// }
 
-function okkk(){
-    let name=window.prompt("what is your name")
-    window.alert(name)
-}
+// function okkk(){
+//     let name=window.prompt("what is your name")
+//     window.alert(name)
+// }
+   
+ let value1=parseInt(prompt("enter the first value"))
+ let value2=parseInt(prompt("enter the second value"))
+ alert(value1+value2)
