986c439a77e6a8496928a31a7469c5c197d74bf7
Yash135-gif <gyash4098@gmail.com>
2025-01-21 11:40:23 +0530

    new code

--- string.js
+++ string.js
@@ -1,5 +1,13 @@
-let string="this is a string"
-document.write(string.toUpperCase())
-document.write(string.toLowerCase())
+// let string="this is a string"
+// document.write(string.toUpperCase())
+// document.write(string.toLowerCase())
 
-  
+
+// let abc="this is a string"
+// console.log(abc.split(" "))
+// let ABC="thi-s is -a st-ring"
+// console.log(ABC.split('-'))
+
+   
+let a="Cybrom is loacted at cybrom in Cybrom is good"
+document.write(a.replace(/cybrom/gi,"MP-Nagar"))
