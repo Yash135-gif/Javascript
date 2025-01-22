d574740a5fb9deabdd59e115e442c5a61204c101
Yash135-gif <gyash4098@gmail.com>
2025-01-22 22:43:59 +0530

    new code

--- Arrays/array.js
+++ Arrays/array.js
@@ -1,9 +1,29 @@
 
-  let fruits=['apple',"mango",'banana'] 
-  fruits.unshift("grapes","pineapple") 
-  document.write(fruits)
+  // let fruits=['apple',"mango",'banana'] 
+  // fruits.unshift("grapes","pineapple") 
+  // document.write(fruits)
   
+  // let fruits=["apple","mango","banana","ornage"]
+
+  // console.log(frutis.length)
+
+  // console.log(fruits.pop())
+  // console.log(fruits)
+
+  // console.log(fruits.push("pineapple","grapes"))
+  // console.log(fruits)
+
+  // let cricketers=["Mucullum","ABD","SKY"]
+  //  console.log(cricketers.shift())
+  //  console.log(cricketers)
+  //  console.log(cricketers.unshift("John Cena","Bumrah"))
+  //  console.log(cricketers)
+
   
+
+
+
+
     
 
 
