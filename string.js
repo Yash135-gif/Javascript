967b8112f4116e939f99e975358cd4a50b9c1aa7
Yash135-gif <gyash4098@gmail.com>
2025-01-22 11:37:33 +0530

    new code

--- string.js
+++ string.js
@@ -9,5 +9,36 @@
 // console.log(ABC.split('-'))
 
    
-let a="Cybrom is loacted at cybrom in Cybrom is good"
-document.write(a.replace(/cybrom/gi,"MP-Nagar"))
+// let a="Cybrom is loacted at cybrom in Cybrom is good"
+// document.write(a.replace(/cybrom/gi,"MP-Nagar"))
+
+// let str1="Javascript is soo easy. javascript is good."
+// let store=str1.replace(/javascript/gi,"CSS")                     // Replace string method 
+// console.log(store)
+
+// let sInA=str1.split(" ")
+// console.log(sInA)
+
+// let piece=str1.slice(4,23)
+// console.log(piece)
+
+// let find=str1.charAt(4)
+// console.log(find)
+
+// let findA=str1.charCodeAt(4)
+// console.log(findA)
+
+// let findB=str1.indexOf(6)
+// console.log(findB)
+
+//    let str2="     this is a string       "
+//    let ans=str2.trim()
+//    console.log(ans)
+
+  
+
+
+
+
+
+
