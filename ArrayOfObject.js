febbc597656845789445d7de9742ea83b1a0875d
Yash135-gif <gyash4098@gmail.com>
2025-02-04 21:51:24 +0530

    new code

--- ArrayOfObject.js
+++ ArrayOfObject.js
@@ -1,10 +1,107 @@
-let Aoo=[{},{},{},{}]
-let array=[2,3,5,6,2,4]
-array.forEach((e)=>{
-console.log(e*2)
-console.log(typeof(e*2))
-}
-)
+// let Aoo=[{},{},{},{}]
+// let array=[2,3,5,6,2,4]
+// array.forEach((e)=>{
+// console.log(e*2)
+// console.log(typeof(e*2))
+// }
+// )
+
+   
+
+// let data=[
+//     {
+//         name:"yash1",
+//         age:21,
+//         city:"Bhopal"
+//     },
+//    {
+//     name:"yash2",
+//     age:21,
+//     city:"Indore"
+//    },
+//    {
+//     name:"yash3",
+//     age:21,
+//     city:"Hydrabad"
+//    }
+// ]
+  
+// let show=document.querySelector("#container")
+
+// data.map((e)=>{
+//     show.innerHTML+=
+    
+//     `<h1>${e.name}  </h1>
+//      <h1> ${e.age}  </h1>
+//     `
+// })
+
+let students=[
+    {
+        name:"yash1",
+        age:21,
+        city:"Bhopal",
+        hobby:"reading"
+    },
+    {
+        name:"yash2",
+        age:22,
+        city:"Indore",
+        hobby:"tennis"
+    },
+    {
+        name:"yash3",
+        age:23,
+        city:"Hydrabad",
+        hobby:"swimming"
+    },
+    {
+        name:"yash4",
+        age:24,
+        city:"Pune",
+        hobby:"writting"
+
+    }
+]
+
+   let show=document.querySelector(".container")
+   show.innerHTML=`  
+   <thead>
+    <tr> 
+
+    <td> Name </td>  
+    <td> Age </td>
+    <td> City </td>
+    <td> Hobby </td>
+
+    </tr> 
+    </thead>`
+    
+
+
+   students.map((e)=>{
+    show.innerHTML+=
+   
+    `  <tr>
+   <td> ${e.name} </td>
+    <td> ${e.age}  </td>
+    <td> ${e.city} </td>
+    <td> ${e.hobby} </td>
+
+     </tr>
+
+    `
+    
+   })
+
+
+
+    
+
+
+
+
+
 
 
 
--- js.js
+++ js.js
@@ -23,6 +23,181 @@
 // x=10
 // console.log(x)
 
+//    Array of objects ---------------------------------
+
+let arr=[
+    {
+        name1:"yash",
+        age1:20,
+        class:10
+    },
+    {
+        name2:"yash2",
+        age2:21,
+        class:11
+    },
+    {
+        name3:"yash3",
+        age3:22,
+        class:12
+    },
+   {
+      name4:"yash4",
+      age4:23,
+      class:13
+   }
+
+]
+
+
+    // ------------------------------ Map ,filter, for Each --------------------------------
+
+    let a1=[4,7,9,2,5,7,6]
+
+    //    Map 
+
+
+    // let ans=a1.map((e)=>{
+    //     return e*2
+    // }
+    // )
+
+
+//    let a1=[2,3,4,6,5,7,5]
+//    let ans=a1.map((e)=> e*2)
+
+//   console.log(ans)
+
+//    filter 
+
+//    let ans2=a1.filter((e)=>
+// {
+//      return e>5
+// }
+// )
+
+  
+//   let ans2=a1.filter((e)=>{
+//      return e%2==0
+//   }
+// )
+
+// console.log(ans2)
+
+//   for Each 
+
+//  a1.forEach((e)=>{
+//    console.log(e*2)
+//  }
+// )
+
+  
+//   let students=[
+//     {
+//         name:"shaurya",
+//         class:9,
+//         fav:"science"
+//     },
+
+//     {
+//         name:"priya",
+//         class:10,
+//         fav:"Maths"
+//     },
+
+//     {
+//         name:"jadoo",
+//         class:9,
+//         fav:"Hindi"
+//     },
+
+//     {
+//         name:"Megha",
+//         class:10,
+//         fav:"English"
+//     }
+//   ]
+
+//    let show=document.querySelector(".container")
+    
+//    students.map((e)=>{
+        
+//       show.innerHTML+=` 
+//         <div>
+//         <h1> ${e.name} </h1>
+//         <h3> ${e.fav} </h3>
+//         <h2> ${e.class} </h2>
+//         </div>
+//       `
+//    })
+
+   
+
+// const colors=["yellow", "green","black","white", "orange"]
+
+//  colors.forEach((e)=>{
+//    console.log(e)
+// })
+
+
+// let store=colors.map((e)=>{
+//     return e
+// })
+// console.log(store)
+
+
+// let months=["january","feburary","march","april","may"]
+// let ok=months.filter((e,index)=>{
+//     return index>=3
+// })
+
+// console.log(ok)
+
+let students=[
+    {
+        name:"Akash",
+        age:21
+    },
+    {
+        name:"Adarsh",
+        age:17
+    },
+    {
+        name:"Amir",
+        age:18
+    },
+    {
+        name:"Raman",
+        age:23
+    },
+    {
+        name:"Nidhi",
+        age:16
+    },
+]
+
+let ans=students.filter((e)=>
+{
+     
+      return e.age>=18
+     
+} 
+).map((e)=>{
+    return e.name
+}).filter((e)=>{
+    return e.includes("A")
+})
+
+console.log(ans)
+
+
+
+
+
+
+
+
+
 
 
 
